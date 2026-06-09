b2be0639df909e4dcc5b4e808042cbce3575e3a6
Diablo <daniel.lomholt.2000@gmail.com>
2026-06-09 14:01:45 +0200

    Make example.py have a path pointing to tests/test.instr to make workflow localize it

--- tests/example.py
+++ tests/example.py
@@ -1,11 +1,14 @@
 import mcstasscript as ms
 
+from pathlib import Path
+
+
 
 instr = ms.McStas_instr("test_mcstas_to_cad", author="Daniel Lomholt Christensen",
                         origin="University of Copenhagen")
 
 
-file = ms.McStas_file("./test.instr")
+file = ms.McStas_file("tests/test.instr")
 
 file.add_to_instr(instr)
 
