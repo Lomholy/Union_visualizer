1f2a50c90f076b0f7bc1ba463630f17029096739
Diablo <daniel.lomholt.2000@gmail.com>
2026-09-16 11:23:22 +0200

    Remove try clause around instrument load, since it conceals errors

--- src/preprocess.py
+++ src/preprocess.py
@@ -45,17 +45,13 @@ def get_union_geometries(instr: ms.McStas_instr):
 
 
 def load_McStas_file(input_file):
-    try:
-        if input_file.endswith(".py"):
-            instr = execute_mcstasscript_file(input_file)
-        elif input_file.endswith(".instr"):
-            file = ms.McStas_file(input_file)
-            instr = ms.McStas_instr("union_cad")
-            file.add_to_instr(instr)
-        return instr
-    except Exception as e:
-        print(e)
-        return
+    if input_file.endswith(".py"):
+        instr = execute_mcstasscript_file(input_file)
+    elif input_file.endswith(".instr"):
+        file = ms.McStas_file(input_file)
+        instr = ms.McStas_instr("union_cad")
+        file.add_to_instr(instr)
+    return instr
 
 
 
